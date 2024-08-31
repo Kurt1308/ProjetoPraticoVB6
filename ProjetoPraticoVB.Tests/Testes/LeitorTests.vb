83ac938db90ac1495a0b6a4abb0e75e620d7f42e
lucasdias <lucasdias@WNB034097BHZ>
2024-08-31 20:03:51 -0300

    Correção classe de teste do Leitor.

--- ProjetoPraticoVB.Tests/Testes/LeitorTests.vb
+++ ProjetoPraticoVB.Tests/Testes/LeitorTests.vb
@@ -1,29 +1,66 @@
-﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+﻿Imports System
 Imports System.IO
 Imports ProjetoPraticoVB6.Leitor
+Imports ProjetoPraticoVB6.Logs
+Imports ProjetoPraticoVB6.Pessoa
+Imports ProjetoPraticoVB6.Serviços
 
-<TestClass>
-Public Class LeitorTests
-    <TestMethod>
-    Public Sub TestLerArquivos_ValidDirectory()
-        Dim filePath As String = "C:\Files\Testes"
-        Directory.CreateDirectory(filePath)
-        File.WriteAllText(Path.Combine(filePath, "test.txt"), "João;Silva;30;Masculino;São Paulo")
+Namespace Leitor
+    Public Class Leitor
+        Implements ILeitor
 
-        Dim leitor As New Leitor()
-        leitor.LerArquivos(filePath)
+        Private ReadOnly _logger As IGeraLog
+        Private ReadOnly _verificadorDiretorio As IVerificadorDiretorio
+        Private ReadOnly _exibidorResultados As IExibidorResultados
+        Private ReadOnly _processadorArquivo As IProcessadorArquivo
 
-        ' Aqui você pode adicionar asserções para verificar se o comportamento esperado ocorreu
-        ' Por exemplo, verificar se o log foi gerado corretamente ou se as contagens estão corretas.
+        Public Sub New(logger As IGeraLog, verificadorDiretorio As IVerificadorDiretorio, exibidorResultados As IExibidorResultados, processadorArquivo As IProcessadorArquivo)
+            _logger = logger
+            _verificadorDiretorio = verificadorDiretorio
+            _exibidorResultados = exibidorResultados
+            _processadorArquivo = processadorArquivo
+        End Sub
 
-        Directory.Delete(filePath, True) ' Limpa o diretório de teste
-    End Sub
+        Public Sub LerArquivos(filePath As String) Implements ILeitor.LerArquivos
+            If Not VerificarDiretorio(filePath) Then
+                Return
+            End If
 
-    <TestMethod>
-    Public Sub TestLerArquivos_InvalidDirectory()
-        Dim leitor As New Leitor()
-        leitor.LerArquivos("C:\Files\DiretorioInvalido")
+            Dim arquivos As String() = ObterArquivos(filePath)
+            If arquivos.Length = 0 Then
+                _logger.Log("Nenhum arquivo .txt encontrado no diretório especificado.")
+                Return
+            End If
 
-        ' Verifique se o log de erro foi gerado corretamente
-    End Sub
-End Class
+            Dim todasPessoasFemininas As List(Of Pessoa) = ProcessarArquivos(arquivos)
+
+            If todasPessoasFemininas.Count > 0 Then
+                _exibidorResultados.ExibirResultados(todasPessoasFemininas)
+            Else
+                _logger.Log("Nenhuma pessoa feminina encontrada nos arquivos.")
+            End If
+        End Sub
+
+        Private Function VerificarDiretorio(diretorio As String) As Boolean
+            Return _verificadorDiretorio.VerificarDiretorio(diretorio)
+        End Function
+
+        Private Function ObterArquivos(diretorio As String) As String()
+            Return Directory.GetFiles(diretorio, "*.txt")
+        End Function
+
+        Private Function ProcessarArquivos(arquivos As String()) As List(Of Pessoa)
+            Dim contadorTotal As Integer = 0
+            Dim todasPessoasFemininas As New List(Of Pessoa)()
+
+            For Each arquivo As String In arquivos
+                Dim pessoasFemininas As List(Of Pessoa) = _processadorArquivo.ProcessarArquivo(arquivo, contadorTotal)
+                If pessoasFemininas IsNot Nothing Then
+                    todasPessoasFemininas.AddRange(pessoasFemininas)
+                End If
+            Next
+
+            Return todasPessoasFemininas
+        End Function
+    End Class
+End Namespace
